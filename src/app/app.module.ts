7f26c9ff748cdb6e85829934a5de2d955550ec45
Tanner Sirota <sirotat@uci.edu>
2019-03-16 13:00:37 -0700

    Adds login and register component, adds authentication, adds auth guard, and token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { RentalModule } from './rental/rental.module';
+import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
     {path: '', redirectTo: '/rentals', pathMatch: 'full'},
@@ -19,9 +19,9 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     RouterModule.forRoot(routes),
-    RentalModule
+    RentalModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
